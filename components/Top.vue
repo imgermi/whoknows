58c779bb71b7972e8e81b69aeb5665634b580ac9
imgermi <juanmartingermano@gmail.com>
2018-12-19 10:20:08 -0300

    Responsive home + menu

--- components/Top.vue
+++ components/Top.vue
@@ -3,8 +3,12 @@
 		<div class="container">
 			<nuxt-link to="/"><img src="~assets/images/whoknows-n.svg" alt="WhoKnows"></nuxt-link>
 			<nav>
-				<button class="hamburger">Menu</button>
-				<ul>
+				<button :class="'hamburger hamburger--spin hamburger--collapse' + (menuActivo ? ' is-active' : '')" type="button" v-on:click="toggleMenu">
+				  <span class="hamburger-box">
+				    <span class="hamburger-inner"></span>
+				  </span>
+				</button>
+				<ul :class="menuActivo ? 'is-active' : ''">
 					<li>
 						<nuxt-link to="/">Inicio</nuxt-link>
 					</li>
@@ -22,3 +26,18 @@
 		</div>
 	</header>
 </template>
+
+<script>
+	export default {
+		data () {
+			return {
+				menuActivo: false
+			}
+		},
+		methods: {
+			toggleMenu: function () {
+			  this.menuActivo = !this.menuActivo
+			},
+		}
+	};
+</script>
