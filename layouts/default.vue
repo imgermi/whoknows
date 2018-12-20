80ac706f57c043be76407b64efd75dd6e9538c6a
imgermi <juanmartingermano@gmail.com>
2018-12-20 10:37:58 -0300

    Ajusto body style

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,14 @@
 </template>
 
 <style>
+	body {
+		background: #F1F1F1;
+		color: $lightBlack;
+		font-size: 16px;
+		font-family: zenon, sans-serif;
+		font-weight: 400;
+		line-height: 24px;
+	}
 	* {
 		margin: 0;
 		padding: 0;
