91b6a819f1678bf3cdc626ec6134e6af821de37d
imgermi <juanmartingermano@gmail.com>
2019-04-27 16:49:19 -0300

    folder blog

---
+++ pages/blog/agencia-remota-2019-diseno-marketing.vue
@@ -0,0 +1,144 @@
+<template>
+	<main class="blog-post">
+		<Top/>
+		<section class="hero">
+			<div class="container">
+				<div class="hero__content">
+					<h1 class="h__hero">Una agencia de diseño y marketing 100% remota en 2019</h1>
+					<h2 class="sub__hero">1/5/19 - Por <a href="https://instagram.com/imgermi" target="_blank">Germi</a></h2>
+				</div>
+			</div>
+		</section>
+		<section class="banda blog-body">
+			<div class="container">
+				<p>El home office es un concepto que ya dejó de ser popular, porque vino para quedarse. Cada vez más empresas le ofrecen a su equipo la posibilidad de trabajar al menos una vez por semana desde casa y, como era de esperarse, esto no deja de ser relevante para el equipo. Cada vez que surge la posibilidad de trabajar desde casa, es clave subir una story a Instagram con algun GIF relacionado.</p>
+				<p>Si, hacer home office garpa. Imaginate si tu trabajo fuese 100% a distancia? Imaginate si, de repente, viene un superior de tu empresa y te dice 'vamos a cerrar las oficinas. A partir de mañana, no vuelvas más. Quedate en tu casa'.</p>
+				<p>Sería loco, no?</p>
+				<p>Bueno, WhoKnows es una empresa 100% remota. Literalmente remota. Y lo divertido es lo siguiente: es una empresa exactamente igual en muchos aspectos a cualquier empresa tradicional. Pero es remota.</p>
+				<p>Podríamos estar horas hablando sobres los pros y los contras del trabajo remoto, y creeme cuando te digo que hay muchos de ambos. Pero ese no es el objetivo de este artículo.</p>
+				<p>Lo que me encantaría es mostrarte cómo es que WhoKnows funciona y opera día a día de manera fluida, siendo una empresa remota.</p>
+				<p>(Lo de fluido a veces es utópico, hay muchos contratiempos. Existimos hace poco, aún tenemos mucho por aprender).</p>
+				<p>((Igual me encanta trabajar remoto)).</p>
+				<p>Vamos a lo importante.</p>
+				<p>Creo que la forma más fácil de explicarte cómo trabajamos de manera remota es dividiendo mi explicación en los puntos/aspectos que dividen a la empresa. Es decir, aquellas cosas con las que nos topamos todos los días, pero de manera categorizada.</p>
+				<p>Estas son las herramientas de las que vamos a hablar:</p>
+				<ul>
+					<li><a href="#slack">Slack</a> para reemplazar a la oficina</li>
+					<li><a href="#asana">Asana</a> para reemplazar al pizarrón lleno de post-it</li>
+					<li><a href="#dropbox">Dropbox & Dropbox Paper</a> para reemplazar los discos externos, pendrives y papeles escritos a mano</li>
+					<li><a href="#loom">Loom</a> para vernos las caras más seguido</li>
+					<li><a href="#figma">Figma</a> para diseñar lo que sea de manera colaborativa</li>
+					<li><a href="#figma">Prospero</a> para hacer contratos</li>
+					<li><a href="#wave">Wave</a> para manejar las finanzas, ingresos y gastos, y tener un control de todos los números</li>
+				</ul>
+				<h3 id="slack">Cuál es la oficina?</h3>
+				<p>Si, en WhoKnows trabajamos desde donde queremos. Pero este punto no se refiere a la cuestión física. Se refiere a la conexión que genera una oficina entre todas las personas.</p>
+				<p>Cómo reemplazamos ese momento en el cual hay un par de personas sentadas en la oficina y de repente llega alguien y se pone a saludar a quienes ya estaban trabajando?</p>
+				<p>Esto es lo dificil. Lo espontaneo de la comunicación.</p>
+				<p>Para esto usamos <a href="https://slack.com" target="_blank">Slack</a>, y de manera abusiva.</p>
+				<p>Por ejemplo, en mi caso, cada vez que me siento a trabajar por primera vez en el día, abro Slack, y le escribo algún mensaje ridículo en mayúscula a algún miembro del equipo. Ejemplo:</p>
+				<p class="quote">"HEYYYYYY GUD MORNINGGGGGGG"</p>
+				<p>Además, aprovechamos los canales para dividirnos en proyectos y equipos. Obviamente tenemos el canal común en donde estamos todos los miembros de WhoKnows, que usamos para compartirnos artículos, links útiles, cosas graciosas, etc.</p>
+				<p>El objetivo clave de Slack: brindar un canal de comunicación que evite mandar mensajes de WhatsApp. Es importantísimo separar la vida personal del trabajo cuando trabajamos de manera remota, porque es muy fácil caer en la tentación de mandarte audios interminables por WhatsApp.</p>
+				<p>En WhoKnows están prohibidos.</p>
+				<h3 id="asana">Y qué onda con los proyectos?</h3>
+				<p>Cada vez que un cliente nuevo entra a WhoKnows, lo primero que hacemos es crear un proyecto nuevo en <a href="https://asana.com" target="_blank">Asana</a> para ese cliente.</p>
+				<p>Asana es un project manager que vive en la nube, y nos permite, de manera colaborativa, organizar a nuestros clientes y proyectos por separado. Cada proyecto tiene sus tareas, sus descripciones, sus fechas de entrega, sus comentarios, etc.</p>
+				<p>Lo genial de Asana es que, al igual que son Slack, podemos aportar al objetivo de EVITAR mandarnos mensajes de WhatsApp. Acá desapareció eso de mandar un WhatsApp para avisarle a alguien 'che, te dejé algo para hacer en Asana'.</p>
+				<p>En Asana podemos crear una tarea, asignarsela a alguien y, si es necesario, etiquetamos a ese alguien y le dejamos un comentario.</p>
+				<p>Con eso nos sobra.</p>
+				<p>El problema que tiene Asana es que rápidamente puede transformarse en un caos. Por eso lo importante es mantener una estructura unificada y consistente entre todos los proyectos, para que cada vez que abramos un proyecto, veamos algo similar y familiar a los ojos.</p>
+				<p>Este es un screenshot de nuestro Asana hoy (mayo 2019):</p>
+				<img src="" alt="">
+				<p>Para estar al pie de la letra todos los dias, aprovechamos la sección Team Conversations de Asana y, al final de cada día, Alex y yo escribimos una reseña de lo que sucedió con los proyectos en curso.</p>
+				<p>Si, todos los días pasa algo distinto en cada proyecto y en cada interacción con cada cliente. Cada cosa buena y mala que sucede hay que contarla.</p>
+				<h3 id="dropbox">Ok, todo genial. Con los archivos de cada proyecto cómo hacen?</h3>
+				<p>Mi primer trabajo fue en un estudio de diseño. Absolutamente todos los archivos estaban guardados en un disco duro común, al cual accedían todas las computadoras. Si un día ese 'servidor' se caía (una vez por semana), nadie podía trabajar.</p>
+				<p>Fue divertido mientras duró.</p>
+				<p>Usamos <a href="https://dropbox.com" target="_blank">Dropbox</a> para organizar todos los archivos de los proyectos. No hay mucho por contar acá, es simplemente una herramienta en la nube que nos permite almacenar archivos.</p>
+				<p>El highlight principal, en mi workflow, es la posibilidad de sincronizar Dropbox con mi computadora y usarlo como si fuese una carpeta local. Al hacer esto, tengo la posibilidad de elegir qué carpetas sincronizar de Dropbox (tenemos 1TB en la nube, y hay muchas cosas que no vale la pena a veces que ocupen lugar en nuestra computadora).</p>
+				<p>Cuando se cierra un proyecto, dejo de sincronizar la carpeta de ese proyecto con mi compu y listo, no la veo más.</p>
+				<p>Obviamente contamos con la seguridad de que todo está en la nube, por lo que no se pierde nada de ningún proyecto.</p>
+				<h3>Y qué hacen para comunicarse mejor con sus clientes?</h3>
+				<p>Acá es donde entra <a href="https://dropbox.com/paper" target="_blank">Dropbox Paper</a>.</p>
+				<p>Una vez que un cliente aprueba una propuesta de trabajo y paga, hacemos un documento compartido en Dropbox Paper que siempre tiene la palabra 'Onboarding' en el título.</p>
+				<p>Es decir, le hacemos un onboarding al cliente a....WhoKnows. De manera virtual. Porque oficina no hay, te acordás?</p>
+				<p>Esta es una de las mejores maneras que tenemos al día de hoy de darle una cálida bienvenida a un nuevo cliente a nuestra agencia. A veces ponemos fotos, a veces algún sticker divertido y, por sobre todas las cosas, emojis. Usamos MUCHOS emojis. Porque rompen el hielo (especialmente cuando las cosas se ponen tensas).</p>
+				<p>En este documento compartido ponemos un timeline (que es el timeline que le mostramos al cliente en la propuesta de trabajo), y luego vinculamos ese timeline con un listado de tareas. Por lo general dividimos nuestros proyectos por etapas (semanas), y cada etapa tiene su propio listado de tareas.</p>
+				<p>De esta manera, el cliente puede ver en qué estamos trabajando, y qué es lo último que hicimos.</p>
+				<p>Esto último se da porque, además de todo lo mencionado, usamos el documento compartido para hacer gran parte de las entregas.</p>
+				<p>Por qué? Porque evita los mails eternos e interminables. Dropbox Paper tiene un sistema de notificaciones que te envía un mail cada vez que alguien hace algo en el documento.</p>
+				<p>Esto es 10 veces mejor que enviarse opiniones y feedback por mail. No te lo recomiendo.</p>
+				<h3 id="loom">Pero en este documento el cliente no te ve la cara, no es poco humano?</h3>
+				<p>Muy cierto. Para esto usamos <a href="https://loom.com" target="_blank">Loom</a>. Y lo usamos MUCHÍSIMO.</p>
+				<p>Loom es una extensión de Chrome que te permite hacer un video muy simple: captura tu pantalla (ese es el foco del video), y en el margen inferior del video agrega un video pequeño tipo avatar de vos mientras te filma la webcam. No es genial?</p>
+				<p>Muy rápidamente y de manera espontanea, podés hacerle un reporte a un cliente para explicarle algo mientras le podés mostrar ese reporte de manera visual (acordate que el video es una captura de tu pantalla).</p>
+				<p>El highlight de Loom: ni bien terminás de grabar el video, te lo guarda en un link en la nube.</p>
+				<p>En WhoKnows tenemos adicción infinita por Loom. Lo usamos para todo. Para pitchear un proyecto cuando hacemos sales, para hacer una propuesta (todas las propuestas empiezan con un Loom), para hacer una entrega o un reporte, y hasta para explicarnos o mostrarnos cosas de manera interna.</p>
+				<p><i>Dicen las malas lenguas que una vez Germi, el director de Diseño, le hizo un Loom a Alex, el director de Marketing, para mostrarle cómo hacer una factura E en el sistema de Afip. Épico.</i></p>
+				<h3 id="figma">Hablaste sobre propuestas, cómo las hacen?</h3>
+				<p>Excelente pregunta para matar dos pájaros de un tiro y contarte sobre dos herramientas. Para esto usamos <a href="https://figma.com" target="_blank">Figma</a> y <a href="https://goprospero.com" target="_blank">Prospero</a>.</p>
+				<p>Figma es una herramienta de diseño (tipo Sketch, Adobe XD, InVision Studio, o Photoshop en las viejas épocas). Además, sirve para prototipar productos como sitios o aplicaciones mobile.</p>
+				<p>Qué diferencia tiene Figma con el resto de las herramientas de diseño? Es una web app, y vive en la nube. Es decir, cada uno de los proyectos que creás en Figma, los podés compartir con un link.</p>
+				<p>Como Figma es una herramienta de diseño, además de usarla para diseñar sitios web, aplicaciones, posteos para redes, entre otras cosas, la usamos para crear propuestas a modo de presentación.</p>
+				<p>La propuestas de trabajo de WhoKnows son una presentación A4 horizontal, nada revolucionario. Así es como se ven a simple vista:</p>
+				<p>Por lo general, tienen al menos entre 5 y 7 páginas, e incluyen (gracias a la magia del prototipado) un botón para abrir el contrato de Prospero.</p>
+				<p>Prospero es una web app que sirve para crear propuestas de trabajo (o proposals), pero en nuestro caso la usamos a modo de contrato.</p>
+				<p>Al igual que Figma, tiene la magia de que los contratos se pueden compartir simplemente con un link. Entonces, una vez que terminamos la propuesta en Figma, creamos un contrato sencillo con el precio de la propuesta y los términos y condiciones (cada día más exigentes).</p>
+				<p>Al final del contrato, el cliente cuenta con la posibilidad de firmarlo de manera digital.</p>
+				<p>De esta manera dejamos por escrito todos los puntos del proyecto asentados, y firmados por el cliente.</p>
+				<p>Lo interesante de nuestras propuestas es el pequeño funnel que creamos: enviamos un link de un Loom explicando lo global de la propuesta y mostrando las páginas más importantes. Debajo del Loom, ponemos el link del Figma. En el Figma, ponemos un link del Prospero.</p>
+				<p>Cuando un cliente abre el Loom, recibimos una notificación. Cuando un cliente abre el Prospero, también.</p>
+				<p>Y si lo firma, también recibimos una notificación (y descorchamos un champagne).</p>
+				<h3 id="wave">Cómo manejan las finanzas?</h3>
+				<p>Hacemos un seguimiento de todos los movimientos de dinero con una aplicación llamada <a href="https://www.waveapps.com/" target="_blank">Wave</a>.</p>
+				<p>Es una web app creada para pequeñas empresas (como WhoKnows).</p>
+				<p>En Wave podemos cargar a nuestros clientes, servicios, y miembros del equipo. Cada vez que un cliente paga, lo cargamos en Wave. Si el cliente pagó a través de un medio de pago que cobra comisión (como Paypal o MercadoPago), lo cargamos en Wave. Cada vez que le pagamos a algún miembro de WhoKnows, lo cargamos en Wave. Cada vez que pagamos un servicio (como Prospero), lo cargamos en Wave.</p>
+				<p>Básicamente, cargamos absolutamente todo en Wave.</p>
+				<p>Lo interesante de Wave es que nos permite tener un panorama sobre el revenue (es decir los ingresos) y profit (es decir, los ingresos menos los gastos) de WhoKnows. En el dashboard principal, Wave nos muestra varios gráficos a modo de reporte en donde podemos visualizar cómo le fue a WhoKnows el mes pasado, o en los últimos 3 meses, o cuando nosotros querramos.</p>
+				<h3>Qué recomendación le darías a alguien que está por empezar?</h3>
+				<p>Empezar.</p>
+				<p>Todo lo que te conté en este artículo es una construcción de prueba y error no planificada. Cada una de las cosas que hacemos día a día fueron surgiendo a veces por necesidad, y otras por ganas de mejorar o innovar de manera interna.</p>
+				<p>Lo importante es no casarse con algunas cosas:</p>
+				<ul>
+					<li>Con lo que hace el resto. Si WhoKnows usa estas herramientas, eso no significa que sirvan para vos.</li>
+					<li>Con los productos y herramientas por fanatismo. Lo que a vos te sirva, está bien.</li>
+				</ul>
+				<p><strong>El consejo más grande que te puedo dar: no planifiques absolutamente nada sobre tu negocio o futuro negocio. Las necesidades van a surgir recién una vez que empieces. Antes de empezar, si usas Slack o no es irrelevante.</strong></p>
+			</div>
+		</section>
+		<Foot/>
+	</main>
+</template>
+
+<script>
+import Top from '~/components/Top.vue'
+import Foot from '~/components/Foot.vue'
+
+export default {
+  components: {
+    Top,
+    Foot
+  },
+  data () {
+    return {
+      title: 'Una agencia de diseño y marketing 100% remota en 2019 | WhoKnows Agency - Agencia Digital',
+      description: 'El home office es un concepto que ya dejó de ser popular, porque vino para quedarse. Cada vez más empresas le ofrecen a su equipo la posibilidad de trabajar al menos una vez por semana desde casa y, como era de esperarse, esto no deja de ser relevante para el equipo.'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.description },
+        { property: 'og:title', content: this.title },
+        { property: 'og:description', content: this.description},
+        { property: 'og:url', content: 'https://thewhoknows.com/blog/agencia-remota-2019-diseno-marketing' },
+      ],
+      link: [
+        { rel: 'canonical', href: 'https://thewhoknows.com/blog/agencia-remota-2019-diseno-marketing' },
+      ],
+    }
+  }
+};
+</script>
