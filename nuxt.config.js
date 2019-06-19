d9306c2d63b406a41681f4e5db7e39d64a0e0bdb
imgermi <juanmartingermano@gmail.com>
2019-06-19 19:25:08 -0300

    Cambio en favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,11 +22,11 @@ module.exports = {
       { property: 'twitter:card', content: 'summary_large_image' },
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href: 'favicon/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon/favicon-16x16.png' },
-      { rel: 'manifest', href: 'favicon/site.webmanifest' },
-      { rel: 'mask-icon', href: 'favicon/safari-pinned-tab.svg', color: '#2f2a95' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicon/site.webmanifest' },
+      { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#2f2a95' },
       { rel: 'stylesheet', href: 'https://use.typekit.net/meo8vpa.css' }
     ],
     script: [
