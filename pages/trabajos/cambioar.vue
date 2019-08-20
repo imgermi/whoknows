e6e95420ba936497397cb7ea0d1b0ec4cb787fc7
imgermi <juanmartingermano@gmail.com>
2019-08-20 14:08:18 -0300

    CambioAr case

---
+++ pages/trabajos/cambioar.vue
@@ -0,0 +1,182 @@
+<template>
+	<main class="trabajo">
+		<Top/>
+		<section class="hero">
+			<div class="container">
+				<div class="hero__content">
+					<h1 class="h__hero">CambioAR</h1>
+					<h2 class="sub__hero" style="max-width: 560px">Cómo ayudamos a una fintech emergente a crear un sitio para comprar y vender dólares</h2>
+				</div>
+			</div>
+		</section>
+		<section class="banda banda-first banda-last trabajo__text">
+			<div class="container">
+				<div class="row">
+					<div class="col-6 left">
+						<h3 class="trabajo__text--h">"La primera agencia autorizada de cambio online de Argentina"</h3>
+						<ul class="trabajo__text--tasks">
+							<p>Lo que hicimos:</p>
+							<li>UX/UI</li>
+							<li>Product Strategy</li>
+							<li>UI Development</li>
+							<li>Ads design</li>
+						</ul>
+					</div>
+					<div class="col-6 right">
+						<p class="trabajo__text--p">Así es como ayudamos a CambioAR, un emprendimiento argentino, a crear una aplicación web para comprar y vender dólares.</p>
+						<p class="trabajo__text--p">Formamos un equipo junto a los founders y developers de la empresa y trabajamos paso a paso para resolver los distintos problemas que existían. CambioAR contaba con la tecnología desarrollada, pero le faltaba darle vida comercial a ese desarrollo para poder lanzarlo al mercado y comenzar a captar usuarios.</p>
+						<a href="https://cambioar.com" target="_blank" class="btn">Visitar sitio</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="trabajo__banner">
+			<img src="~assets/images/trabajos/cambioar/cambioar-comprar-dolares-home.jpg" alt="CambioAR - Homepage cambioar.com">
+		</section>
+		<section class="banda banda-first banda-last trabajo__text">
+			<div class="container">
+				<div class="row">
+					<div class="col-6 left">
+						<h3 class="trabajo__text--h">Project kick-off</h3>
+					</div>
+					<div class="col-6 right">
+						<p class="trabajo__text--p">Comenzamos a trabajar en este proyecto con el siguiente desafío: la empresa ya se encontraba trabajando junto a un equipo de desarrollo y tenía una buena parte del producto ya armado desde la parte funcional.</p>
+						<p class="trabajo__text--p">Con un back-end alimentando un prototipo realizado con Bootstrap, tuvimos que amoldarnos a gran parte del flujo de navegación que el equipo había diagramado.</p>
+						<p class="trabajo__text--p">Esto es lo que CambioAR necesitaba de nosotros:
+							<ul>
+								<li>Diseñar el producto a nivel UX/UI desde lo realizado por los desarrolladores</li>
+								<li>Desarrollar las interfaces de forma 100% responsive y hacer un hand-off al equipo de desarrollo</li>
+								<li>Llevar el desarrollo desde una idea hasta un producto comercial listo para salir al mercado</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="trabajo__banner">
+			<img src="~assets/images/trabajos/cambioar/cambioar-comprar-dolares-paleta.jpg" alt="CambioAR - Paleta de colores utilizada">
+		</section>
+		<section class="banda banda-first banda-last trabajo__text">
+			<div class="container">
+				<div class="row">
+					<div class="col-6 left">
+						<h3 class="trabajo__text--h">Desde las palabras hasta lo visual</h3>
+					</div>
+					<div class="col-6 right">
+						<p class="trabajo__text--p">A partir de un trabajo realizado con el equipo de CambioAR 100% enfocado en el copywriting que iba a tener el sitio, comenzamos a volcar las palabras sobre distintos layouts en wireframes de baja fidelidad.</p>
+						<p class="trabajo__text--p">A partir de estos wireframes y de un relevamiento de sitios de referencia, empezamos a definir la estructura visual del producto:</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="trabajo__banner">
+			<img src="~assets/images/trabajos/cambioar/cambioar-comprar-dolares-wireframes.jpg" alt="CambioAR - Wireframes cambioar.com">
+		</section>
+		<section class="banda banda-first banda-last trabajo__text">
+			<div class="container">
+				<div class="row">
+					<div class="col-6 left">
+					</div>
+					<div class="col-6 right">
+						<p class="trabajo__text--p">Con una estructura visual definida, el proyecto empezó a tomar su rumbo hacia lo que se convertiría en el producto final.</p>
+						<p class="trabajo__text--p">Con enfoque en mantener a la marca presente durante toda la experiencia, comenzamos a darle vida al diseño visual del producto.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="trabajo__banner">
+			<img src="~assets/images/trabajos/cambioar/cambioar-comprar-dolares-ui.jpg" alt="CambioAR - UI final del sitio">
+		</section>
+		<section class="banda banda-first banda-last trabajo__text">
+			<div class="container">
+				<div class="row">
+					<div class="col-6 left">
+						<h3 class="trabajo__text--h">Desarrollando las interfaces</h3>
+					</div>
+					<div class="col-6 right">
+						<p class="trabajo__text--p">Una vez que cerramos el diseño del producto en su totalidad, comenzamos a desarrollar las interfaces.</p>
+						<p class="trabajo__text--p">Considerando que necesitábamos feedback constante no solo de los founders de CambioAR sino también del equipo de desarrolladores trabajando en el producto, deployamos nuestra propia versión del sitio en Netlify.</p>
+						<p class="trabajo__text--p">Gracias a esto, el producto se podía testear pantalla por pantalla, desde cualquier navegador, y desde cualquier dispositivo, sin la necesidad de pasar por todo el proceso de volcar el desarrollo de las interfaces sobre el front-end real del sitio.</p>
+						<p class="trabajo__text--p">Utilizando la versión final de las interfaces desarrolladas como referencia visual, acompañamos al equipo de desarrollo mientras ejecutaba nuestras interfaces en la aplicación real, para llegar a un resultado final responsive que respete los guidelines visuales originales.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="trabajo__banner">
+			<img src="~assets/images/trabajos/cambioar/cambioar-comprar-dolares-mockups.jpg" alt="CambioAR - Mockups de algunas pantallas en versión responsive">
+		</section>
+		<section class="banda banda-first trabajo__testimonio">
+			<div class="container">
+				<article>
+					<h3 class="trabajo__text--h">Son Argentinos, invirtieron u$s500.000 y ahora venden “dólar barato” por internet</h3>
+					<p style="margin-top: 10px">Se trata de CambioAR, un emprendimiento de un HSBC y un ex Microsoft.</p>
+					<a href="https://www.infotechnology.com/negocios/Son-argentinos-invirtieron-us-500.000-y-ahora-venden-dolar-barato-por-internet-20190701-0004.html" target="_blank" class="btn">Leer nota</a>
+				</article>
+			</div>
+		</section>
+		<section class="banda banda-last">
+			<div class="container">
+				<h3 class="h__section text-center">Proyectos relacionados</h3>
+				<div class="index__trabajos">
+				  <article class="caseStudy">
+				    <nuxt-link to="/trabajos/enchufarte">
+				      <img src="~assets/images/trabajos/enchufarte/mock-small.jpg" alt="">
+				      <div class="caseStudy__text">
+				        <p class="caseStudy__text--h">Enchufarte México</p>
+				        <span class="caseStudy__text--sub">Ecommerce, Ads</span>
+				      </div>
+				    </nuxt-link>
+				  </article>
+				  <article class="caseStudy">
+				    <nuxt-link to="/trabajos/mru">
+				      <img src="~assets/images/trabajos/mru/mock-small.jpg" alt="">
+				      <div class="caseStudy__text">
+				        <p class="caseStudy__text--h">MRU México</p>
+				        <span class="caseStudy__text--sub">Ecommerce, Ads</span>
+				      </div>
+				    </nuxt-link>
+				  </article>
+				</div>
+				<div class="text-center">
+				  <nuxt-link to="/trabajos" class="btn">Ver más proyectos</nuxt-link>
+				</div>
+			</div>
+		</section>
+		<Contact/>
+		<Foot/>	
+	</main>
+</template>
+
+<script>
+import Top from '~/components/Top.vue'
+import Contact from '~/components/Contact.vue'
+import Foot from '~/components/Foot.vue'
+
+export default {
+  components: {
+    Top,
+    Contact,
+    Foot
+  },
+  data () {
+    return {
+      title: 'CambioAR | WhoKnows Agency - Agencia de Diseño y Marketing',
+      description: 'Así es como ayudamos a CambioAR, una fintech argentina, a crear una aplicación web para comprar y vender dólares.'
+    }
+  },
+  head () {
+  	return {
+  		title: this.title,
+  		meta: [
+  		  { hid: 'description', name: 'description', content: this.description },
+  		  { property: 'og:title', content: this.title },
+  		  { property: 'og:description', content: this.description},
+  		  { property: 'og:url', content: 'https://thewhoknows.com/trabajos/cambioar' },
+  		],
+      link: [
+        { rel: 'canonical', href: 'https://thewhoknows.com/trabajos/cambioar' },
+      ],
+  	}
+  }
+};
+</script>
